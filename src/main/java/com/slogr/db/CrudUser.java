6534160584a3bb3374e3e5fd11d84f7d5dc2448d
Vatsalgame <Vatsalgame@gmail.com>
2015-04-19 17:13:08 -0700

    Added api call and db call to get all users from the user collection.

--- src/main/java/com/slogr/db/CrudUser.java
+++ src/main/java/com/slogr/db/CrudUser.java
@@ -1,11 +1,15 @@
 package com.slogr.db;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.slogr.core.User;
 import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class containing all the methods for crud ops with the User collection/object
@@ -18,6 +22,10 @@ public class CrudUser {
 
     private static MongoDriver driver = new MongoDriver();
 
+    /**
+     * Add the given user to the users collection
+     * @param user to be added
+     */
     public void createUser(User user) {
         MongoDatabase db = driver.getDb();
         MongoCollection<Document> userCollection = db.getCollection("users");
@@ -26,6 +34,25 @@ public class CrudUser {
         document.append("password", user.getPassword());
 
         userCollection.insertOne(document);
+    }
+
+    public List<User> getAllUsers() {
+        MongoDatabase db = driver.getDb();
+        MongoCollection<Document> userCollection = db.getCollection("users");
+        MongoCursor<Document> userCursor = userCollection.find().iterator();
+
+        List<User> users = new ArrayList<User>();
+
+        try {
+            while(userCursor.hasNext()) {
+                Document userDocument = userCursor.next();
+                users.add(new User(userDocument.getString("email"), userDocument.getString("password")));
+            }
+        } finally {
+            userCursor.close();
+        }
+
+        return users;
     }
 
 }
